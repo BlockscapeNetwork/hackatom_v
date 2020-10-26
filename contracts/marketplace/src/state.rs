fb0909cf42996bc408bc4f776fc4eae3afd632c7
Douglas Sondheim <douglas.sondheim@mway.io>
2020-10-26 17:07:08 +0100

    define state

--- contracts/marketplace/src/state.rs
+++ contracts/marketplace/src/state.rs
@@ -1,7 +1,8 @@
+use cw_storage_plus::Map;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::{CanonicalAddr};
+use cosmwasm_std::{CanonicalAddr, Coin};
 
 pub static CONFIG_KEY: &[u8] = b"config";
 
@@ -10,3 +11,18 @@ pub struct State {
     pub count: i32,
     pub owner: CanonicalAddr,
 }
+
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+pub struct Offering {
+    pub token_id: String,
+
+    pub contract_addr: CanonicalAddr,
+
+    pub seller: CanonicalAddr,
+
+    pub list_price: Coin,
+}
+
+// pub const TOKENS: Map<&str, TokenInfo> = Map::new(b"tokens");
+pub const OFFERINGS: Map<(&[u8], &[u8]), Offering> = Map::new(b"offerings");
