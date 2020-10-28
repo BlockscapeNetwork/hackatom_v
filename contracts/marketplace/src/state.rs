89b4276b5480f02703ca81ade9c79ed69b895102
Douglas Sondheim <douglas.sondheim@mway.io>
2020-10-28 12:18:38 +0100

    change offerings key to u64

--- contracts/marketplace/src/state.rs
+++ contracts/marketplace/src/state.rs
@@ -25,7 +25,7 @@ pub struct Offering {
 }
 
 /// OFFERINGS is a map which maps the offering_id to an offering. Offering_id is derived from OFFERINGS_COUNT.
-pub const OFFERINGS: Map<&[u8], Offering> = Map::new(b"offerings");
+pub const OFFERINGS: Map<&u64, Offering> = Map::new(b"offerings");
 
 pub const OFFERINGS_COUNT: Item<u64> = Item::new(b"num_offerings");
 
@@ -39,26 +39,26 @@ pub fn increment_offerings<S: Storage>(storage: &mut S) -> StdResult<u64> {
     Ok(val)
 }
 
-pub struct OfferingIndexes<'a, S: Storage> {
-    pub seller: MultiIndex<'a, S, Offering>,
-    pub contract: MultiIndex<'a, S, Offering>,
-}
+// pub struct OfferingIndexes<'a, S: Storage> {
+//     pub seller: MultiIndex<'a, S, Offering>,
+//     pub contract: MultiIndex<'a, S, Offering>,
+// }
 
-impl<'a, S: Storage> IndexList<S, Offering> for OfferingIndexes<'a, S> {
-    fn get_indexes(&'_ self) -> Box<dyn Iterator<Item = &'_ dyn Index<S, Offering>> + '_> {
-        let v: Vec<&dyn Index<S, Offering>> = vec![&self.seller, &self.contract];
-        Box::new(v.into_iter())
-    }
-}
+// impl<'a, S: Storage> IndexList<S, Offering> for OfferingIndexes<'a, S> {
+//     fn get_indexes(&'_ self) -> Box<dyn Iterator<Item = &'_ dyn Index<S, Offering>> + '_> {
+//         let v: Vec<&dyn Index<S, Offering>> = vec![&self.seller, &self.contract];
+//         Box::new(v.into_iter())
+//     }
+// }
 
-pub fn offerings<'a, S: Storage>() -> IndexedMap<'a, &'a [u8], Offering, S, OfferingIndexes<'a, S>> {
-    let indexes = OfferingIndexes {
-        seller: MultiIndex::new(|o| o.seller.to_vec(), b"offerings", b"offerings__seller"),
-        contract: MultiIndex::new(
-            |o| o.contract_addr.to_vec(),
-            b"offerings",
-            b"offerings__contract",
-        ),
-    };
-    IndexedMap::new(b"offerings", indexes)
-}
+// pub fn offerings<'a, S: Storage>() -> IndexedMap<'a, &'a [u8], Offering, S, OfferingIndexes<'a, S>> {
+//     let indexes = OfferingIndexes {
+//         seller: MultiIndex::new(|o| o.seller.to_vec(), b"offerings", b"offerings__seller"),
+//         contract: MultiIndex::new(
+//             |o| o.contract_addr.to_vec(),
+//             b"offerings",
+//             b"offerings__contract",
+//         ),
+//     };
+//     IndexedMap::new(b"offerings", indexes)
+// }
