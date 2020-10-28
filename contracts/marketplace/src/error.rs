eeae7bca182930f82be4210200f7d584a7cb8965
Fabian Bockaj <fabian.bockaj@mway.io>
2020-10-28 11:57:51 +0100

    Added NoData error

--- contracts/marketplace/src/error.rs
+++ contracts/marketplace/src/error.rs
@@ -6,6 +6,9 @@ pub enum ContractError {
     #[error("{0}")]
     Std(#[from] StdError),
 
+    #[error("No data in ReceiveMsg")]
+    NoData {},
+
     #[error("Unauthorized")]
     Unauthorized {},
     // Add any other custom errors you like here.
